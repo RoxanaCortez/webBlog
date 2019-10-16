a3f8a1a24520b8f25d6528d1a94c30563070ae83
RoxanaCortez <00502211@uca.edu.sv>
2019-10-16 15:18:14 -0600

    Cambio de contenido

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-let flag = false;
 let days = document.querySelectorAll(".days");
+let flag = false;
 let selected;
 
 for (let index = 0; index < days.length; index++) {
-    dias[index].addEventListener("click", () => {
+    days[index].addEventListener("click", () => {
         let link = days[index].getAttribute("href").split('#');
         let day_id = link[link.length-1];
         
         let selected_day = document.getElementById(day_id);
-        selected_day.classList.toggle("hide_elment");
+        selected_day.classList.toggle("hide");
 
         if (flag) {
-            selected.classLifst.toggle("hide_element");
+            selected.classList.toggle("hide");
         }
-        selected = selected_day;
+        
+       selected = selected_day;
         flag = true;
     });
 };
 
-dias[0].click();
+days[0].click();
 
